d5c690412b813169240f5444a9cce45dfd16fb90
Popov-Nikolay <kolya20081224@gmail.com>
2026-09-10 12:25:08 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main() {
+        int a = 0;
+        int r = 1;
+        for (int i  = 1; i <= a; i++) {
+            r *= i;
+        }
+        System.out.println(r);
+    }
+}
